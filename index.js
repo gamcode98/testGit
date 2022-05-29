c8bc0b538ea7629e7d2e65cf919c8236ca3b6258
gabriel <gamcode98@gmail.com>
2022-05-29 17:43:21 -0300

    something new in master

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ console.log("Hello")
 
 const create = async () => {
     const newUser = await createUSer()
+    const palabra = "que choquen las lineas a ver que pasa"
+    const suma = 2+2
+    
     return newUser
 }
 
