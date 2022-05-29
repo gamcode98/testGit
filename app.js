a55ea025500eb98bcd71bfa6dd25aada1a967282
gabriel <gamcode98@gmail.com>
2022-05-29 18:39:56 -0300

    added differents lines of code

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const { port } = require("./config");
 const { connection } = require("./config/db");
-
+const jobs = require("./routes/jobs.routes"); 
+const categories = require("./routes/categories.routes"); 
+const countries = require("./routes/countries.routes"); 
 
 connection();
 
@@ -9,7 +11,9 @@ const app = express();
 
 app.use(express.json());
 
-
+jobs(app); 
+categories(app);
+countries(app); 
 
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
