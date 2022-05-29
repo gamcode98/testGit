294b25004230ce92caf2ea647343fd0109482def
gabriel <gamcode98@gmail.com>
2022-05-29 18:37:49 -0300

    added lines in this branch

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const { port } = require("./config");
 const { connection } = require("./config/db");
-
+const auth = require("./routes/auth.routes");
+const offers = require("./routes/offer.routes");
+const users = require("./routes/user.routes");
 
 connection();
 
@@ -9,7 +11,9 @@ const app = express();
 
 app.use(express.json());
 
-
+users(app);
+auth(app);
+offers(app);
 
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
