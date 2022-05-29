c4e8054a46d293c83ccb9aabea664a3d8a551f68
gabriel <gamcode98@gmail.com>
2022-05-29 18:26:43 -0300

    app.js ready to work

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const { port } = require("./config");
+const { connection } = require("./config/db");
+
+
+connection();
+
+const app = express();
+
+app.use(express.json());
+
+
+
+app.listen(port, () => {
+  console.log(`Listening on port: ${port}`);
+});
