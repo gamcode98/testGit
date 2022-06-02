314c6bccf28882b598e3599f1503fe91ab850a85
gabriel <gamcode98@gmail.com>
2022-06-02 11:31:18 -0300

    Revert "before some changes"
    This reverts commit e3dd12efe4233e8acc29f2a72d66afa253d9fc4c.

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,8 @@
-const router = require('express').Router();
-const usersRouter = require('./users');
-const authRouter = require('./auth');
-const membersRouter = require('./members');
-
+var express = require('express');
+var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
-
-router.use('/users', usersRouter);
-router.use('/auth', authRouter);
-router.use('/members', membersRouter);
-
 
 module.exports = router;
